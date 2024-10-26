6b638f2897f00c41aaea3956de73afb173e4b2bf
trilochankumar9 <trilochankumar9@gmail.com>
2024-10-26 09:52:57 +0530

    modal

--- resources/views/admin/Auth/my_wallet.blade.php
+++ resources/views/admin/Auth/my_wallet.blade.php
@@ -83,4 +83,132 @@
       </div> 
 
       </div>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Add Money</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+      
+      
+              <form>
+                <div class="row gy-3">
+                  <div class="col-12">
+                    <label>Name</label>
+                    <input type="text" class="form-control" placeholder="Enter name">
+                  </div>
+                  <div class="col-12"> 
+                    <label>Amount</label>
+                    <input type="text" class="form-control" placeholder="Enter amount">
+                  </div>
+      
+                  <div class="col-12"> 
+                    <label>Description</label>
+                    <textarea class="form-control"></textarea>
+                  </div>
+                  <div class="col-6"> 
+                    <label>Payment Option</label>
+                    <select class="form-select">
+                      <option>Cash</option>
+                    </select>
+                  </div>
+                  <div class="col-6"> 
+                    <label>Payment Option</label>
+                    <select class="form-select">
+                      <option>Cash</option>
+                    </select>
+                  </div>
+      
+                  <div class="col-12 text-end"> 
+                    <input type="submit" value="submit" class="btn btn-primary">
+                  </div>
+      
+                </div>
+              </form>
+        
+            </div>
+          </div>
+        </div>
+      </div>
+      
+        
+       <!-- Modal market-->
+       <div class="modal fade" id="createindent" tabindex="-1" aria-labelledby="createindent" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="marketLabel">System Info</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form class="common_form">
+                <div class="row gy-3">
+                  <div class="col-6">
+                    <label>Business Name</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-6">
+                    <label>System Name</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-12">
+                    <label>Address</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-4">
+                    <label>Email Address</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-4">
+                    <label>Contact Number</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-4">
+                    <label>Currency</label>
+                    <input type="text" class="form-control">
+                  </div>
+                  <div class="col-6">
+                    <label>Logo 1</label>
+                    <input type="file" class="form-control">
+                  </div>
+                  <div class="col-6"> 
+                    <label>Logo 2</label>
+                    <input type="file" class="form-control">
+                  </div>
+                  <div class="col-12 text-end"> 
+                    <input type="submit" value="update" class="btn btn-primary">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="modal fade" id="restore" tabindex="-1" aria-labelledby="restore" aria-hidden="true"> 
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="marketLabel">Restore Database</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="row gy-3">
+                  <div class="col-12">
+                    <label>Upload Sql file</label>
+                    <input type="file" class="form-control">
+                  </div>
+                  <p>Note: pls upload sql file only.</p>
+                  <div class="col-12 text-end"> 
+                    <input type="submit" value="Restore" class="btn btn-primary">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
